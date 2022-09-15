5983024976616c62481e2370cac1f941fbc889b1
AliHaider18 <alixhaider7@gmail.com>
2022-09-15 20:37:53 +0500

    AdminController

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -5,14 +5,13 @@ const registerController = require('../controllers/adminController');
 router.post('/student', registerController.addNewStudent);
 router.post('/teacher', registerController.addNewTeacher);
 
-router.put('/student', registerController.addNewStudent);
-router.put('/teacher', registerController.addNewTeacher);
+router.put('/student', registerController.updateStudent);
+router.put('/teacher', registerController.updateTeacher);
 
-router.delete('/student', registerController.addNewStudent);
-router.delete('/teacher', registerController.addNewTeacher);
+router.delete('/student', registerController.deleteStudent);
+router.delete('/teacher', registerController.deleteTeacher);
 
-router.get('/getAllStudents', registerController.addNewStudent);
-router.get('/getAllTeachers', registerController.addNewTeacher);
-
+router.get('/getAllStudents', registerController.getAllStudent);
+router.get('/getAllTeachers', registerController.getAllTeacher); 
 
 module.exports = router;
