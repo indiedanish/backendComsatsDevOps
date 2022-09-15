1caed6c8ebe17fcad9a571087d3998c74049d182
indiedanish <danishbuttdb@hotmail.com>
2022-09-15 08:07:11 +0500

    Env file added

--- routes/register.js
+++ routes/register.js
@@ -5,4 +5,5 @@ const registerController = require('../controllers/registerController');
 router.post('/student', registerController.addNewStudent);
 router.post('/teacher', registerController.addNewTeacher);
 
+
 module.exports = router;
